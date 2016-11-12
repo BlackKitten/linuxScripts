56754213d8356b928f3df11ff4205f3e26dafb33
BenBertrands <ben.bertrands@gmail.com>
2016-11-12 16:08:08 +0100

    hasContent finished

--- hasContent.sh
+++ hasContent.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+pattern=$1
+while read file;
+    do
+        [ -f "$file" ] &&
+            if grep -qE $pattern "$file"
+            then
+                echo $file;
+            fi
+done
